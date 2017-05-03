a984f4168ac906056cbe88d0770792e01e3e5a8e
Mark-hernandez <Mark-hernandez@outlook.com>
2017-05-02 21:34:21 -0700

    added about and home with podcast episodes

---
+++ app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+
+
+$("p").hide();
+
+$(".about").click(function() {
+  $("p").show();
+  });
+
+$(".about").click(function() {
+  $("iframe").hide();
+});
+
+$(".home").click(function() {
+  $("p").hide();
+  $("iframe").show();
+});
+
+
+});
+
+
